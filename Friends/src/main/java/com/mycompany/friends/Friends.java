8f34166a4a0b0e2a9027c163ab7e879ebfe59c4d
SoraHrS <soherreras@unal.edu.co>
2025-02-19 17:49:08 -0500

    friends

---
+++ Friends/src/main/java/com/mycompany/friends/Friends.java
@@ -0,0 +1,9 @@
+package com.mycompany.friends;
+
+public class Friends {
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal = new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true); 
+        miVentanaPrincipal.setLocationRelativeTo(null);
+    }
+}
